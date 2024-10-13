9178c686767d401b1b415ee5db4bc48afdef4020
Sxy623 <shenxinyi@zju.edu.cn>
2024-10-14 01:15:42 +0800

    Text and Image

---
+++ Image.swiftpm/ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("paris")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300)
+            .overlay(
+                Color.black
+                    .opacity(0.4)
+                    .overlay(
+                        Text("Paris")
+                            .font(.largeTitle)
+                            .fontWeight(.black)
+                            .foregroundStyle(.white)
+                            .frame(width: 200)
+                    )
+            )
+        
+    }
+}
