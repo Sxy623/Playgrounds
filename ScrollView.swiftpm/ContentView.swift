dcfdd20bd40ddec4bac4e6f296afcb27071f7232
Sxy623 <shenxinyi@zju.edu.cn>
2024-10-15 22:45:31 +0800

    Scroll View

---
+++ ScrollView.swiftpm/ContentView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("MONDAY, AUG 20")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    Text("Your Reading")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                }
+                Spacer()
+            }
+            .padding([.top, .horizontal])
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top) {
+                    Group {
+                        CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                        CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                        CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                        CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                    }
+                    .frame(width: 300)
+                }
+            }
+        }
+    }
+}
