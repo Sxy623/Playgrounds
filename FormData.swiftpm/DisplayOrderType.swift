769ada5de588197a1e108f6b3f4ee47cfd3d874d
Sxy623 <shenxinyi@zju.edu.cn>
2024-10-30 16:00:51 +0800

    Form data

---
+++ FormData.swiftpm/DisplayOrderType.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+enum DisplayOrderType: Int, CaseIterable {
+    case alphabetical = 0
+    case favoriteFirst = 1
+    case checkInFirst = 2
+    
+    init(type: Int) {
+        switch type {
+        case 0: self = .alphabetical
+        case 1: self = .favoriteFirst
+        case 2: self = .checkInFirst
+        default: self = .alphabetical
+        }
+    }
+    
+    var text: String {
+        switch self {
+        case .alphabetical: return "Alphabetical"
+        case .favoriteFirst: return "Show Favorite First"
+        case .checkInFirst: return "Show Check-in First"
+        }
+    }
+    
+    func predicate() -> ((Restaurant, Restaurant) -> Bool) {
+        switch self {
+        case .alphabetical: return { $0.name < $1.name }
+        case .favoriteFirst: return { $0.isFavorite && !$1.isFavorite }
+        case .checkInFirst: return { $0.isCheckIn && !$1.isCheckIn }
+        }
+    }
+}
