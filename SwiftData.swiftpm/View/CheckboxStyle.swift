f819fc618188af07344ed17249f7119f69807d89
Sxy623 <shenxinyi@zju.edu.cn>
2024-11-27 21:01:57 +0800

    SwiftData

---
+++ SwiftData.swiftpm/View/CheckboxStyle.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct CheckboxStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        return HStack {
+            Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+                .resizable()
+                .frame(width: 24, height: 24)
+                .foregroundStyle(configuration.isOn ? .purple : .gray)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+            configuration.label
+        }
+    }
+}
